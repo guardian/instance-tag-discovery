99a486638de686c1adb74aef1935f72ecded8d75
Simon Hildrew <simon@hildrew.net>
2021-05-12 16:26:28 +0100

    Correct the architecture

--- script/build.sh
+++ script/build.sh
@@ -2,7 +2,7 @@
 set -e
 mkdir target
 build() {
-  PKG_ROOT="target/instance-tag-discovery-${LINUXARCH}_1.0-1"
+  PKG_ROOT="target/instance-tag-discovery-${GOARCH}_1.0-1"
   mkdir -p ${PKG_ROOT}/usr/bin ${PKG_ROOT}/DEBIAN
   go build -o ${PKG_ROOT}/usr/bin/instance-tag-discovery .
   cat > ${PKG_ROOT}/DEBIAN/control <<EOF
@@ -10,7 +10,7 @@ Package: instance-tag-discovery
 Version: 1.0-1
 Section: base
 Priority: optional
-Architecture: ${LINUXARCH}
+Architecture: ${GOARCH}
 Maintainer: DevX <devx@theguardian.com>
 Description: Writes out instance tags at boot time
 EOF
@@ -20,5 +20,5 @@ EOF
     echo "Skipping packaging as dpkg-deb command doesn't exist"
   fi
 }
-GOOS=linux GOARCH=amd64 LINUXARCH=x86_64 build
-GOOS=linux GOARCH=arm64 LINUXARCH=aarch64 build
+GOOS=linux GOARCH=amd64 build
+GOOS=linux GOARCH=arm64 build
