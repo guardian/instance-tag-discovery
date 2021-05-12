9cdaf319bbc2435f9265cab7c03ae638084b165d
Simon Hildrew <simon@hildrew.net>
2021-05-12 16:19:17 +0100

    Fix pkg option

--- script/build.sh
+++ script/build.sh
@@ -15,7 +15,7 @@ Maintainer: DevX <devx@theguardian.com>
 Description: Writes out instance tags at boot time
 EOF
   if hash dpkg-deb 2>/dev/null; then
-    dpkg-deb -build ${PKG_ROOT}
+    dpkg-deb --build ${PKG_ROOT}
   else
     echo "Skipping packaging as dpkg-deb command doesn't exist"
   fi
