d6378b1f5b3fd05bb92b224fd17f592abd028340
Nicolas Long <nicolas.long@theguardian.com>
2022-03-28 17:00:06 +0100

    Read IMDS (instance metadata) for tags
    Tags are now available via instance metadata:
    
    https://aws.amazon.com/about-aws/whats-new/2022/01/instance-tags-amazon-ec2-instance-metadata-service/
    
    So let's use this and fallback to ASG/EC2 API calls as required.
    Metadata is preferred here as it is quicker to retrieve and doesn't
    suffer from rate limiting unlike API calls.

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
-# Go AWS example
+# Instance Tag Discovery
+
+A simple program to fetch instance tags and write them to disk.
+
+Tags are fetched from (in order of preference):
+
+* instance metadata (IMDS v2 - enable tags via [Launch Template
+  metadata](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ec2-launchtemplate-launchtemplatedata-metadataoptions.html#cfn-ec2-launchtemplate-launchtemplatedata-metadataoptions-instancemetadatatags))
+* the ASG
+* the EC2 instance (EC2 API)
+
+They are written to:
+
+* `/etc/config/tags.properties` (key=value format)
+* `/etc/config/tags.json` (JSON format)
+
+The `out-dir` flag can be used to change the target directory.
+
+**We strongly recommend that you configure your instances to support tags on
+their metadata. This will make lookup quicker and also avoid the possibility of
+hitting rate limiting on the AWS API.**
+
+## Local development
+
+Install Go:
 
     brew install go
 
-Then check your version using `go version`. You'll need 1.15 or greater to use
-the AWS V2 SDK.
+Then run some commands:
+
+    go test
+    go run main.go --profile [profile] --out-dir . --instance-id [some-id]
+
+If unsure about editors, we recommend using VS Code.
 
 Go has in-built dependency management using [Go
 mod](https://blog.golang.org/using-go-modules).
@@ -15,6 +43,8 @@ The AWS SDK for Go has docs here:
 
 # Deployment
 
-This tool is baked into images in the cdk-base role in AMIgo. In order to update it you should grab the built artifacts from the GitHub actions build and upload them to `packages/deb/` in the AMIgo data bucket.
+This tool is baked into images in the cdk-base role in AMIgo. In order to update
+it you should grab the built artifacts from the GitHub actions build and upload
+them to `packages/deb/` in the AMIgo data bucket.
 
 It would be nice to make this work with RiffRaff at some stage.
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
@@ -22,55 +23,54 @@ const fileMode = 0644
 
 func main() {
 	outDirParam := flag.String("out-dir", "/etc/config/", "output directory")
-	instanceIDParam := flag.String("instance-id", "", "aws instance id")
+	instanceIDParam := flag.String("instance-id", "", "AWS instance id")
+	profileParam := flag.String("profile", "deployTools", "AWS credentials profile (useful if running locally)")
 
 	flag.Parse()
-
-	instanceID, err := getInstanceID(*instanceIDParam)
-	check(err, "Error getting instance")
 
 	cfg, err := config.LoadDefaultConfig(
 		context.TODO(),
 		config.WithRegion("eu-west-1"),
-		config.WithSharedConfigProfile("deployTools"),
+		config.WithSharedConfigProfile(*profileParam),
 	)
-	check(err, "Error loading config")
+	check(err, "error loading config")
 
+	_, err = cfg.Credentials.Retrieve(context.TODO())
+	check(err, "unable to retrieve credentials")
+
+	imdsClient := imds.New(imds.Options{})
 	asgClient := autoscaling.NewFromConfig(cfg)
+	ec2Client := ec2.NewFromConfig(cfg)
 
-	asgTags, err := getTagsFromASG(asgClient, instanceID)
+	instanceID, err := getInstanceID(imdsClient, *instanceIDParam)
+	check(err, "error getting instance")
 
-	var tags map[string]string
-	if (err != nil) {
-		// check if err is lack of permission??
-		//check(err, "Error getting tags from ASG")
-		logf("Failed to get tags from ASG, falling back to EC2")
-		ec2Client := ec2.NewFromConfig(cfg)
+	tags, err := tagsFromIMDS(imdsClient)
+	if err != nil {
+		logf("failed to get tags from instance metadata, falling back to ASG. Metadata error was: %v", err)
+		tags, err = getTagsFromASG(asgClient, instanceID)
 
-		tags, err = getTagsFromInstance(ec2Client, instanceID)
-
-		check(err, "Error getting tags from EC2")
-	} else {
-		logf("Tags fetched from ASG")
-		tags = asgTags
+		if err != nil {
+			logf("failed to get tags from ASG, falling back to EC2. ASG error was: %v", err)
+			tags, err = getTagsFromInstance(ec2Client, instanceID)
+		}
 	}
 
-	var fileContent string
-
+	tagProperties := ""
 	for key, value := range tags {
-		fileContent = fileContent + fmt.Sprintf("%s=%s\n", key, value)
+		tagProperties = tagProperties + fmt.Sprintf("%s=%s\n", key, value)
 	}
 
 	tagJSON, err := json.Marshal(tags)
-	check(err, "not json")
+	check(err, "unable to marshall tags as JSON")
 
 	err = os.MkdirAll(*outDirParam, os.ModePerm)
-	check(err, "couldn't create directory")
+	check(err, "couldn't create out directory")
 
 	propertiesPath := path.Join(*outDirParam, propertiesFilename)
 	jsonPath := path.Join(*outDirParam, jsonFilename)
 
-	err = ioutil.WriteFile(propertiesPath, []byte(fileContent), fileMode)
+	err = ioutil.WriteFile(propertiesPath, []byte(tagProperties), fileMode)
 	check(err, "couldn't create properties file")
 
 	err = ioutil.WriteFile(jsonPath, tagJSON, fileMode)
@@ -79,23 +79,49 @@ func main() {
 	logf("Written %d tags to %s and %s", len(tags), propertiesPath, jsonPath)
 }
 
-func getInstanceID(instanceIDParam string) (string, error) {
+func getInstanceID(client *imds.Client, instanceIDParam string) (string, error) {
 	if instanceIDParam != "" {
 		logf("Instance ID %s passed as param", instanceIDParam)
 		return instanceIDParam, nil
 	}
 
-	client := imds.New(imds.Options{})
 	input := imds.GetInstanceIdentityDocumentInput{}
 	output, err := client.GetInstanceIdentityDocument(context.TODO(), &input)
-
 	if err != nil {
 		return "", err
 	}
 
-	logf("Instance ID from IMDS is %s", output.InstanceID)
-
 	return output.InstanceID, nil
+}
+
+func tagsFromIMDS(client *imds.Client) (map[string]string, error) {
+	tags := map[string]string{}
+
+	input := imds.GetMetadataInput{Path: "tags/instance"}
+	resp, err := client.GetMetadata(context.TODO(), &input)
+	if err != nil {
+		return tags, fmt.Errorf("unable to call tags metadata endpoint: %w", err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Content)
+	defer resp.Content.Close()
+	if err != nil {
+		return tags, fmt.Errorf("unable to read body of imds tags request:  %w", err)
+	}
+
+	tagNames := strings.Split(strings.TrimSpace(string(body)), "\n")
+
+	for _, name := range tagNames {
+		input := imds.GetMetadataInput{Path: "tags/instance/" + name}
+		value, _ := client.GetMetadata(context.TODO(), &input)
+		if err == nil {
+			body, _ = ioutil.ReadAll(value.Content)
+			tags[name] = string(body)
+			continue
+		}
+	}
+
+	return tags, err
 }
 
 func getTagsFromASG(client *autoscaling.Client, instanceID string) (map[string]string, error) {
@@ -107,7 +133,7 @@ func getTagsFromASG(client *autoscaling.Client, instanceID string) (map[string]s
 	describeASGInstancesOutput, err := client.DescribeAutoScalingInstances(context.TODO(), &describeASGInstancesInput)
 
 	if err != nil {
-		return response, err
+		return response, fmt.Errorf("unable to describe asg instances: %w", err)
 	}
 
 	if asgInstancesLength := len(describeASGInstancesOutput.AutoScalingInstances); asgInstancesLength != 1 {
@@ -139,23 +165,20 @@ func getTagsFromASG(client *autoscaling.Client, instanceID string) (map[string]s
 func getTagsFromInstance(client *ec2.Client, instanceID string) (map[string]string, error) {
 	response := map[string]string{}
 
-	describeEC2InstanceInput := ec2.DescribeInstancesInput{
-		InstanceIds: []string{instanceID},
-	}
-	describeEC2InstanceOutput, err := client.DescribeInstances(context.TODO(), &describeEC2InstanceInput)
-	
+	input := ec2.DescribeInstancesInput{InstanceIds: []string{instanceID}}
+	output, err := client.DescribeInstances(context.TODO(), &input)
 	if err != nil {
 		return response, err
 	}
 
-	if reservationsLength := len(describeEC2InstanceOutput.Reservations); reservationsLength != 1 {
-		return response, fmt.Errorf("Expected 1 Reservation. Got %v", reservationsLength)
+	reservationsLength := len(output.Reservations)
+	if reservationsLength != 1 {
+		return response, fmt.Errorf("expected 1 Reservation. Got %v", reservationsLength)
 	}
 
-	instances := describeEC2InstanceOutput.Reservations[0].Instances
-
-	if length := len(instances); length != 1 {
-		return response, fmt.Errorf("Expected 1 Instance. Got %v", length)
+	instances := output.Reservations[0].Instances
+	if len(instances) != 1 {
+		return response, fmt.Errorf("expected 1 Instance. Got %v", len(instances))
 	}
 
 	for _, tag := range instances[0].Tags {
